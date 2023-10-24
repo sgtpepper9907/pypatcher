f7e2d048da94abe78d0cd458c2f88008a14ae06c
David Angulo <dangulo@etsy.com>
2023-10-24 16:34:15 -0600

    Create setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pypatcher',
+    packages = ['pypatcher'],
+    version = '0.1',
+    license='GPL-3.0',
+    description = 'Utility module for handling incremental git patches',
+    author = 'David Angulo',
+    author_email = 'david.angulo.arias@gmail.com',
+    url = 'https://github.com/sgtpepper9907/pypatcher',
+    download_url = 'https://github.com/sgtpepper9907/pypatcher/archive/refs/tags/v0.1.tar.gz',
+    keywords = ['git', 'patches', 'incremental'],
+)
